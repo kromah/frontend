70df361eec6b9f72b23b6ef01cd7acc7c04f1412
gabrielkromah <emedialib@gmail.com>
2023-01-30 16:09:16 +0000

    Test3

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,9 +1,9 @@
 import { Swiper } from 'swiper/react';
 import { Pagination } from 'swiper';
-import './Carousel.scss';
+// import './Carousel.scss';
 
-import 'swiper/scss';
-import 'swiper/scss/pagination';
+// import 'swiper/scss';
+// import 'swiper/scss/pagination';
 
 const Carousel = ({ children, slides }) => {
   return (
